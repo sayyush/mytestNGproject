8c1212cb49053320ce705a7382f4ba5203a6f6bc
Sayyara Abdrazakova <s.abdrazakova95@gmail.com>
2023-05-28 23:56:33 +0300

    v2

---
+++ src/test/java/com/myapp/tests/Day18_FirstConfigTest.java
@@ -0,0 +1,17 @@
+package com.myapp.tests;
+
+import com.myapp.utilities.ConfigReader;
+import com.myapp.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class Day18_FirstConfigTest {
+
+    @Test
+    public void firstConfigTest(){
+        Driver.getDriver().get(ConfigReader.getProperty("amazon_url"));
+        String actualTitle= Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.toLowerCase().contains(ConfigReader.getProperty("amazon_title")));
+        Driver.closeDriver();
+    }
+}
+
---
+++ src/test/java/com/myapp/tests/Day18_FirstDriverTest.java
@@ -0,0 +1,26 @@
+package com.myapp.tests;
+
+import com.myapp.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class Day18_FirstDriverTest {
+
+    @Test
+    public void firstDriverTest(){
+//        Go to amazon page
+//        driver.get("https://www.amazon.com");
+//        driver ->>>>> Driver.getDriver()
+        Driver.getDriver().get("https://www.amazon.com");
+
+//        Verify the title includes amazon
+//        driver.getTitle();
+        String actualTitle = Driver.getDriver().getTitle();
+//        assert actualTitle.contains("amazon");
+        Assert.assertTrue(actualTitle.toLowerCase().contains("amazon"));
+
+//        closing the driver
+//        driver.quit();
+        Driver.closeDriver();
+    }
+}
+
